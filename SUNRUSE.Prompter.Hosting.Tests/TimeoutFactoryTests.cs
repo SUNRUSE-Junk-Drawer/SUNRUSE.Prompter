4273628bf56ac88c9a7569d0da3dd958308ad01e
jameswilddev <jameswilddev@outlook.com>
2017-12-29 23:23:37 +0000

    Added a factory interface and implementation for the Timeout class.

---
+++ SUNRUSE.Prompter.Hosting.Tests/TimeoutFactoryTests.cs
@@ -0,0 +1,73 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using Xunit;
+
+namespace SUNRUSE.Prompter.Hosting.Tests
+{
+    public sealed class TimeoutFactoryTests
+    {
+        public sealed class DisposableMock : IDisposable
+        {
+            public int DisposedTimes = 0;
+
+            public void Dispose()
+            {
+                DisposedTimes++;
+            }
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void CreatesATimeout()
+        {
+            var disposable = new DisposableMock();
+            var timeoutFactory = new TimeoutFactory();
+
+            using (var timeout = timeoutFactory.Create(TimeSpan.FromMilliseconds(1500), disposable))
+            {
+
+                Assert.IsType<Timeout>(timeout);
+            }
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void UsesTheGivenDuration()
+        {
+            var disposable = new DisposableMock();
+            var timeoutFactory = new TimeoutFactory();
+
+            using (var timeout = timeoutFactory.Create(TimeSpan.FromMilliseconds(1500), disposable))
+            {
+
+                Assert.Equal(TimeSpan.FromMilliseconds(1500), Assert.IsType<Timeout>(timeout).Duration);
+            }
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void UsesTheGivenDisposable()
+        {
+            var disposable = new DisposableMock();
+            var timeoutFactory = new TimeoutFactory();
+
+            using (var timeout = timeoutFactory.Create(TimeSpan.FromMilliseconds(1500), disposable))
+            {
+
+                Assert.Same(disposable, Assert.IsType<Timeout>(timeout).ToDispose);
+            }
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void DoesNotDisposeOfTheGivenInstance()
+        {
+            var disposable = new DisposableMock();
+            var timeoutFactory = new TimeoutFactory();
+
+            using (var timeout = timeoutFactory.Create(TimeSpan.FromMilliseconds(1500), disposable))
+            {
+
+                Assert.Equal(0, disposable.DisposedTimes);
+            }
+        }
+    }
+}
---
+++ SUNRUSE.Prompter.Hosting/TimeoutFactory.cs
@@ -0,0 +1,27 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+
+namespace SUNRUSE.Prompter.Hosting
+{
+    /// <summary>Creates <see cref="ITimeout"/> instances.</summary>
+    public interface ITimeoutFactory
+    {
+        /// <summary>Creates a new <see cref="ITimeout"/>.</summary>
+        /// <param name="duration">The <see cref="TimeSpan"/> before <paramref name="toDispose"/> is <see cref="IDisposable.Dispose"/>d.</param>
+        /// <param name="toDispose">The <see cref="IDisposable"/> instance to create a <see cref="ITimeout"/> for.</param>
+        /// <returns>A <see cref="ITimeout"/> for <paramref name="duration"/>/<paramref name="toDispose"/>.</returns>
+        ITimeout Create(TimeSpan duration, IDisposable toDispose);
+    }
+
+    /// <inheritdoc />
+    public sealed class TimeoutFactory : ITimeoutFactory
+    {
+        /// <inheritdoc />
+        public ITimeout Create(TimeSpan duration, IDisposable toDispose)
+        {
+            return new Timeout(duration, toDispose);
+        }
+    }
+}
