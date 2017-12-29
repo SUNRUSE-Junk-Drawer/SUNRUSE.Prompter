c812f51ec4f2677edd042670e3ce427eb4588bc9
jameswilddev <jameswilddev@outlook.com>
2017-12-29 23:33:30 +0000

    Added an extension method for installing hosting services.  Currently, this only includes TimeoutFactory.

---
+++ SUNRUSE.Prompter.Hosting.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace SUNRUSE.Prompter.Hosting.Tests
+{
+    public sealed class ServiceCollectionExtensionsTests
+    {
+        [Fact, Trait("Type", "Unit")]
+        public void AddHostingDoesNotChangeExistingRegistrations()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton("Test String");
+            var obj = new object();
+            serviceCollection.AddSingleton(obj);
+
+            serviceCollection.AddHosting();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            Assert.Equal("Test String", serviceProvider.GetRequiredService<string>());
+            Assert.Same(obj, serviceProvider.GetRequiredService<object>());
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void AddHostingReturnsServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            var returned = serviceCollection.AddHosting();
+
+            Assert.Same(serviceCollection, returned);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void AddHostingInjectsTimeoutFactoryAsITimeoutFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddHosting();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            Assert.IsType<TimeoutFactory>(serviceProvider.GetRequiredService<ITimeoutFactory>());
+        }
+    }
+}
---
+++ SUNRUSE.Prompter.Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SUNRUSE.Prompter.Hosting
+{
+    /// <summary>Extension methods for installing hosting services into <see cref="IServiceCollection"/>s.</summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>Installs the shared hosting services into a <see cref="IServiceCollection"/> instance as singletons.</summary>
+        /// <typeparam name="TServiceCollection">The <see cref="IServiceCollection"/> <see cref="Type"/> to install singletons into.</typeparam>
+        /// <param name="serviceCollection">The <typeparamref name="TServiceCollection"/> to install singletons into.</param>
+        /// <returns>The given <paramref name="serviceCollection"/>.</returns>
+        public static TServiceCollection AddHosting<TServiceCollection>(this TServiceCollection serviceCollection) where TServiceCollection : IServiceCollection
+        {
+            serviceCollection.AddSingleton<ITimeoutFactory, TimeoutFactory>();
+            return serviceCollection;
+        }
+    }
+}
